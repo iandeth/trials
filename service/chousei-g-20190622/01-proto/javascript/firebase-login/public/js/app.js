cbda5c4a39351aff98ff8eedb6c25317accc7a25
iandeth <iandeth@gmail.com>
2019-07-07 22:03:16 +0900

    chousei-g-proto: refactor js/html

--- service/chousei-g-20190622/01-proto/javascript/firebase-login/public/js/app.js
+++ service/chousei-g-20190622/01-proto/javascript/firebase-login/public/js/app.js
@@ -1,10 +1,7 @@
 class App {
   constructor() {
-    this.$username = $('#username');
-    this.$inBtn = $('#signin');
-    this.$outBtn = $('#signout');
-    this.$offlineBtn = $('#offline');
-    this.$helloBtn = $('#hello');
+    this.$signOutUserPanel = undefined;
+    this.$signInUserPanel = undefined;
     this.auth = undefined;
     this.gapi = undefined;
   }
@@ -18,51 +15,63 @@ class App {
     this.gapi = new GAPI();
     this.gapi.init();
 
-    this.initUI();
-    this.initSignInCheck();
+    $(()=> {
+      this.initUI();
+      this.initSignInCheck();
+    });
   }
 
   initUI() {
-    this.$inBtn.on('click', ()=> {
+    this.$signOutUserPanel = $('#sign-out-user-panel');
+    this.$signInUserPanel = $('#sign-in-user-panel');
+    var $inBtn = $('#signin');
+    var $outBtn = $('#signout');
+    var $offlineBtn = $('#offline');
+    var $helloBtn = $('#hello');
+
+    $inBtn.on('click', ()=> {
       console.log('signin start');
       this.auth.signIn();
       return false;
-    }).hide();
+    });
 
-    this.$outBtn.on('click', ()=> {
+    $outBtn.on('click', ()=> {
       this.auth.signOut();
       return false;
-    }).hide();
+    });
 
-    this.$offlineBtn.on('click', ()=> {
-      this.gapi.grantOfflineAccess().then((code) => {
+    $offlineBtn.on('click', ()=> {
+      this.gapi.grantOfflineAccess().then((code)=> {
         var goa = firebase.functions().httpsCallable('user_getOfflineAccess');
         goa({ code:code }).then((r)=> { console.log('offline cf', r) });
       });
       return false;
-    }).hide();
+    });
 
-    this.$helloBtn.on('click', ()=> {
+    $helloBtn.on('click', ()=> {
+      $helloBtn.hide();
       var hello = firebase.functions().httpsCallable('misc_helloWorld');
-      hello({ a:1, b:2 }).then((r)=> { console.log('hello', r) });
+      hello({ a:1, b:2 }).then((r)=> {
+        console.log('hello', r);
+        $helloBtn.show();
+      });
       return false;
     });
   }
 
   initSignInCheck() {
     firebase.auth().onAuthStateChanged((user)=> {
+      let $username = $('#username');
       if(user) {
         console.log('signed in', user);
-        this.$username.text('Hi, ' + user.displayName).show();
-        this.$inBtn.hide();
-        this.$outBtn.show();
-        this.$offlineBtn.show();
+        $username.text('Hi, ' + user.displayName);
+        this.$signInUserPanel.show();
+        this.$signOutUserPanel.hide();
       } else {
         console.log('signed out')
-        this.$username.text('').hide();
-        this.$inBtn.show();
-        this.$outBtn.hide();
-        this.$offlineBtn.hide();
+        $username.text('');
+        this.$signInUserPanel.hide();
+        this.$signOutUserPanel.show();
       }
     });
   }
@@ -85,7 +94,7 @@ class Auth {
   }
 
   signOut() {
-    firebase.auth().signOut();
+    return firebase.auth().signOut();
   }
 }
 
