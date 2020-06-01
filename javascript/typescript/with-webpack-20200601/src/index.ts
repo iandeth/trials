0ec31280cb35855f238830c02a52b87fdec698e1
iandeth <iandeth@gmail.com>
2020-06-02 02:33:10 +0900

    add promise, async/await code

--- javascript/typescript/with-webpack-20200601/src/index.ts
+++ javascript/typescript/with-webpack-20200601/src/index.ts
@@ -1,27 +1,46 @@
 import Dog from "src/model/dog";
 
-let msg = "hello";
-console.log(msg);
+const runClasses = () => {
+  let msg = "hello";
+  console.log(msg);
 
-// type error
-//msg = 2;
+  // type error
+  //msg = 2;
 
-const sam = new Dog("sam");
-console.log("name", sam.name);
+  const sam = new Dog("sam");
+  console.log("name", sam.name);
 
-sam.name = "willy";
-console.log("name changed", sam.name);
+  sam.name = "willy";
+  console.log("name changed", sam.name);
 
-const mr = sam.move(2);
-console.log("move return", mr);
+  const mr = sam.move(2);
+  console.log("move return", mr);
 
-sam.bark();
+  sam.bark();
 
-// readonly error
-//sam.legs = 8;
-//sam.name = "sammy";
+  // readonly error
+  //sam.legs = 8;
+  //sam.name = "sammy";
+};
 
-// global jquery working
-$(() => {
-  $("<h2>jquery working</h2>").insertAfter("h1");
-});
+const runJQ = () => {
+  // global jquery working
+  $(() => {
+    $("<h2>jquery working</h2>").insertAfter("h1");
+  });
+};
+
+const runAsync = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve("waited 100ms"), 100);
+  });
+};
+
+// main scope
+Promise.resolve()
+  .then(runClasses)
+  .then(runJQ)
+  .then(async () => {
+    const r = await runAsync();
+    console.log("#runAsync returns:", r);
+  });
--- javascript/typescript/with-webpack-20200601/src/model/dog.ts
+++ javascript/typescript/with-webpack-20200601/src/model/dog.ts
@@ -3,7 +3,6 @@ import Animal from "src/model/animal";
 export default class Dog extends Animal {
   constructor(name: string) {
     super(name, 4);
-
     // readonly error
     //this.legs = 4;
   }
