f2e332c84a373c13ef574905139ea4d47132c650
iandeth <iandeth@gmail.com>
2020-06-20 13:12:44 +0900

    self transpiled chai-as-promised worked

--- javascript/typescript/with-webpack-20200601/test/support/setup.ts
+++ javascript/typescript/with-webpack-20200601/test/support/setup.ts
@@ -1,5 +1,7 @@
 import * as _chai from "chai";
-import * as chaiAsPromised from "chai-as-promised";
+// use require instead of import to avoid typescript's "implicitly has any type" error
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const chaiAsPromised = require("test/support/chai-as-promised");
 _chai.use(chaiAsPromised);
 
 export const chai = _chai;
