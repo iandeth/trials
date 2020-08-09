61b35fdd5660dfc820caff301df6f73b9a1dd3b7
iandeth <iandeth@gmail.com>
2020-08-10 00:29:58 +0900

    remove local ajax-emulate stories

--- javascript/react/road-to-react/hacker-stories/src/App.js
+++ javascript/react/road-to-react/hacker-stories/src/App.js
@@ -1,31 +1,7 @@
 import React from "react";
 import "./App.css";
 
-const initialStories = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
-const getAsyncStories = () => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => resolve({ data: { stories: initialStories } }), 2000);
-    //setTimeout(() => reject(), 2000);
-  });
-};
+const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
 
 const storiesReducer = (state, action) => {
   switch (action.type) {
@@ -83,11 +59,13 @@ const App = () => {
 
   React.useEffect(() => {
     dispatchStories({ type: "STORIES_FETCH_INIT" });
-    getAsyncStories()
+
+    fetch(`${API_ENDPOINT}react`)
+      .then((response) => response.json())
       .then((result) => {
         dispatchStories({
           type: "STORIES_FETCH_SUCCESS",
-          payload: result.data.stories,
+          payload: result.hits,
         });
       })
       .catch(() => dispatchStories({ type: "STORIES_FETCH_FAILURE" }));
