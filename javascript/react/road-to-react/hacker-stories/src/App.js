3a490423095028ca82992911db0b4cf338f6d38a
iandeth <iandeth@gmail.com>
2020-08-10 12:12:24 +0900

    replace with form html

--- javascript/react/road-to-react/hacker-stories/src/App.js
+++ javascript/react/road-to-react/hacker-stories/src/App.js
@@ -88,26 +88,19 @@ const App = () => {
     setSearchTerm(event.target.value);
   };
 
-  const handleSearchSubmit = () => {
+  const handleSearchSubmit = (event) => {
     setUrl(`${API_ENDPOINT}${searchTerm}`);
+    event.preventDefault();
   };
 
   return (
     <div>
       <h1>My Hacker Stories</h1>
-      <InputWithLabel
-        id="search"
-        label="Search"
-        value={searchTerm}
-        isFocused={true}
-        onInputChange={handleSearchInput}
-      >
-        <strong>Search:</strong>
-      </InputWithLabel>
-
-      <button type="button" disabled={!searchTerm} onClick={handleSearchSubmit}>
-        Submit
-      </button>
+      <SearchForm
+        searchTerm={searchTerm}
+        onSearchInput={handleSearchInput}
+        onSearchSubmit={handleSearchSubmit}
+      />
 
       <hr />
       {stories.isError && <p>Something went wrong ...</p>}
@@ -150,6 +143,23 @@ const InputWithLabel = ({
     </>
   );
 };
+
+const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
+  <form onSubmit={onSearchSubmit}>
+    <InputWithLabel
+      id="search"
+      value={searchTerm}
+      isFocused
+      onInputChange={onSearchInput}
+    >
+      <strong>Search:</strong>
+    </InputWithLabel>
+    <button type="submit" disabled={!searchTerm}>
+      {" "}
+      Submit
+    </button>
+  </form>
+);
 
 const List = ({ list, onRemoveItem }) => {
   console.log("List render");
